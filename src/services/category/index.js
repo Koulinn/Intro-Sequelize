a2d8ed2c4a74d22344fd76e03203b7bb6d0d9880
Koulinn <drdverzola@gmail.com>
2021-08-26 15:33:41 +0100

    comment route refactored

--- src/services/category/index.js
+++ src/services/category/index.js
@@ -1,81 +1,18 @@
 import express from "express"
-import db from "../../db/models/db_assoc.js"
-
-const Category = db.Category
-const Product = db.Product
-import s from "sequelize"
-const { Op } = s
+import comment from "./handlers.js"
 
 const router = express.Router()
 
 router
   .route("/")
-  .get(async (req, res, next) => {
-    try {
-      const data = await Category.findAll({
-        include: Product,
-      })
+  .get(comment.getAll)
 
-      res.send(data)
-    } catch (error) {
-      console.log(error)
-      next(error)
-    }
-  })
-
-  .post(async (req, res, next) => {
-    try {
-      console.log('post category ')
-      const data = await Category.create(req.body)
-      console.log(data)
-      res.send(data)
-    } catch (error) {
-      console.log(error)
-      next(error)
-    }
-  })
+  .post(comment.create)
 
 router
   .route("/:id")
-  .get(async (req, res, next) => {
-    try {
-      const data = await Category.findByPk(req.params.id, {
-        include: Product,
-      })
-      res.send(data)
-    } catch (error) {
-      console.log(error)
-      next(error)
-    }
-  })
-  .put(async (req, res, next) => {
-    try {
-      const data = await Category.update(req.body, {
-        where: { id: req.params.id },
-        returning: true,
-      })
-      res.send(data[1][0])
-    } catch (error) {
-      console.log(error)
-      next(error)
-    }
-  })
-  .delete(async (req, res, next) => {
-    try {
-      const rows = await Category.destroy({
-        where: {
-          id: req.params.id,
-        },
-      })
-      if (rows > 0) {
-        res.send("ok")
-      } else {
-        res.status(404).send("not found")
-      }
-    } catch (error) {
-      console.log(error)
-      next(error)
-    }
-  })
+  .get(comment.getSingle)
+  .put(comment.update)
+  .delete(comment.deleteSingle)
 
 export default router
