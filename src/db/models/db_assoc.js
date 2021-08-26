d8a07b619862635150724d2dc1e61ff2015ccb90
Koulinn <drdverzola@gmail.com>
2021-08-26 17:12:47 +0100

    Cart Model and associations created

--- src/db/models/db_assoc.js
+++ src/db/models/db_assoc.js
@@ -3,6 +3,7 @@ import Product from "./Product.js"
 import Category from "./Category.js"
 import User from "./User.js"
 import Comments from "./Comments.js"
+import Cart from "./Cart.js"
 
 Product.belongsTo(Category, { foreignKey: {allowNull: false}})
 Category.hasMany(Product, { foreignKey: {allowNull: false}})
@@ -19,6 +20,13 @@ User.hasMany(Comments, { foreignKey: {allowNull: false}})
 Product.hasMany(Comments, { foreignKey: {allowNull: false}})
 Comments.belongsTo(Product, { foreignKey: {allowNull: false}})
 
+Product.belongsToMany(User, {through: {model:Cart, unique: false}} )
+User.belongsToMany(Product, {through: {model:Cart, unique: false}} )
 
+Product.hasMany(Cart, { foreignKey: {allowNull: false}})
+Cart.belongsTo(Product, { foreignKey: {allowNull: false}})
 
-export default { sequelize, Product, Category, User, Comments }
+User.hasMany(Cart, { foreignKey: {allowNull: false}})
+Cart.belongsTo(User, { foreignKey: {allowNull: false}})
+
+export default { sequelize, Product, Category, User, Comments, Cart }
