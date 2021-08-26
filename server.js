0e0159d68519135c06789014a945199ece775043
Koulinn <drdverzola@gmail.com>
2021-08-26 10:58:46 +0100

    added error handlers and cors config

--- server.js
+++ server.js
@@ -3,15 +3,31 @@ import db from "./src/db/models/db_assoc.js"
 import cors from "cors"
 import categoryRouter from "./src/services/category/index.js"
 import productRouter from "./src/services/products/index.js"
+import lib from "./src/lib/index.js"
+
+const {errorHandlers, serverConfig} = lib
+
 
 const server = express()
 const { PORT } = process.env
 
 server.use(express.json())
-server.use(cors())
+server.use(cors(serverConfig))
 
 server.use("/category", categoryRouter)
 server.use("/product", productRouter)
+
+
+
+
+
+
+server.use(errorHandlers.forbidden)
+server.use(errorHandlers.notFound)
+server.use(errorHandlers.badRequest)
+server.use(errorHandlers.server)
+
+
 
 await db.sequelize.sync()
 
@@ -20,6 +36,7 @@ server.listen(PORT, () => console.log("🚀 Server is running on port ", PORT))
 server.on("error", (error) =>
   console.log("🚀 Server is crashed due to ", error)
 )
+
 
 // const dbConn = async () => await db.sequelize
 //   .sync()
---
+++ src/lib/index.js
@@ -0,0 +1,7 @@
+import errorHandlers from "./error-handlers.js";
+import serverConfig from "./server-config.js";
+
+
+
+
+export default {errorHandlers, serverConfig}
